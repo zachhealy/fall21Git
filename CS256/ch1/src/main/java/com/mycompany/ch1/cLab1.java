40ddace7c2be2f77275f6bbfdf0efbff8ddd2d00
zachhealy <zachjhealy@gmail.com>
2021-08-25 10:21:55 -0500

    commit on 'main'

--- CS256/ch1/src/main/java/com/mycompany/ch1/cLab1.java
+++ CS256/ch1/src/main/java/com/mycompany/ch1/cLab1.java
@@ -9,7 +9,7 @@ public class cLab1 {
 
     public static void main(String[] args){
         boolean array[];
-        array = new boolean[150];
+        array = new boolean[151];
         mailClosed(array);
         everyOther(array);
         checkClosed(array);
