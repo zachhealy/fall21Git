c426df64e31689d0318028d8fcd59e6aefc721b8
zachhealy <zachjhealy@gmail.com>
2021-08-25 10:43:47 -0500

    new example

---
+++ CS256/ch1/src/main/java/com/mycompany/ch1/Ch1_ConsoleInputEx.java
@@ -0,0 +1,57 @@
+/*
+ * This example first reviews the usage of command line arguments
+ *
+ * This example then reviews standard input from console with two approaches
+ * 1. using System.console()
+ * 2. using Scanner(System.in): suppprts directly read in integer, double, etc
+ *
+ * to run the program in console, go to the project folder Ch1_ConsoleInputEx,
+ * find its subfolder: build, and the next subfolder classes
+ * change directory with command cd to classes folder (in windows)
+ * in classes subfolder, run: java ch1_consoleinputex.Ch1_ConsoleInputEx
+ */
+package ch1_consoleinputex;
+
+import java.io.Console;
+import java.util.Scanner;
+
+/**
+ *
+ * @author ziping
+ */
+public class Ch1_ConsoleInputEx {
+    public static void main(String[] args) {
+        System.out.println("Welcome to cs265, here is the args from cmd:");
+        for(String s : args)
+            System.out.println(s);
+
+        //The System.console() will return null in IDE, 
+        //running the class in console or terminal manually        
+        /*Console console = System.console();
+        String input = console.readLine().trim();
+        int num = Integer.parseInt(input);
+        int sum = 0;
+        
+        while (!input.equalsIgnoreCase("quit")) {
+            num = Integer.parseInt(input);
+            sum += num;
+            System.out.println("num: " + num);
+            System.out.print("Enter an integer: ");
+            input = console.readLine().trim();
+        }     
+        System.out.println("the summation is:" + sum);*/
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a positive integer:");
+        int num = scanner.nextInt();
+        int sum = 0;
+        while(num != -99999){
+            sum += num;
+            System.out.println("num: " + num);
+            System.out.print("Enter a positive integer: ");        
+            num = scanner.nextInt();
+        }
+        System.out.println("the summation is:" + sum);
+    }
+    
+}
