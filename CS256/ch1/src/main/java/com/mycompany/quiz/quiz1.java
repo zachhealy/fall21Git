dbbac13ee1467196010ac83df152bc17b022d249
zachhealy <zachjhealy@gmail.com>
2021-08-29 11:38:53 -0500

    commit on 'main'

---
+++ CS256/ch1/src/main/java/com/mycompany/quiz/quiz1.java
@@ -0,0 +1,55 @@
+package com.mycompany.quiz;
+/*  
+Author: Zach Healy
+Date: 8/29/2021
+Purpose: This code will define a playing card, allow the user to access the card info, as well as compare cards to see which is greater.
+*/
+public class quiz1 {
+    public static void main(String[] args) {
+        Card card1 = new Card("Spade", 10);
+        Card card2 = new Card("Diamond", 3);
+        
+        card1.getSuit();
+        card1.getFace();
+        System.out.println(card1.getSuit());
+        System.out.println(card1.getFace());
+        card1.setCard("Heart", 5);
+        System.out.println(card1.getSuit());
+        System.out.println(card1.getFace());
+
+        System.out.println(card1.isLessThan(card2.getFace()));
+    }
+}
+
+class Card{
+    private String suit;
+    private int face;
+
+    public Card(String newSuit, int newFace){
+        suit = newSuit;
+        face = newFace;
+
+    }
+    public String getSuit(){
+        return suit;
+
+    }
+    public int getFace(){
+        return face;
+
+    }
+    public void setCard(String newSuit, int newFace){
+        suit = newSuit;
+        face = newFace;
+    }
+    public boolean isLessThan(int C){
+        if(face > C){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+
+}
