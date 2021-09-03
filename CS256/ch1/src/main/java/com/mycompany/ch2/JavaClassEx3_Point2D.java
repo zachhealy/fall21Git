fd65e14a8432ab4692f45167c9c0278f73aa06e6
zachhealy <zachjhealy@gmail.com>
2021-09-03 11:19:28 -0500

    commit on 'main'

--- CS256/ch1/src/main/java/com/mycompany/ch2/JavaClassEx3_Point2D.java
+++ CS256/ch1/src/main/java/com/mycompany/ch2/JavaClassEx3_Point2D.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaclassex3_point2d;
+package com.mycompany.ch2;
 
 //import javafx.geometry.Point2D;
 
