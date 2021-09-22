8d10920be0ed2486cc4163fdde879c11a92af2e5
zachhealy <zachjhealy@gmail.com>
2021-09-22 14:44:13 -0500

    "he's kinda being a weiner about it"

---
+++ CS256/ch1/src/main/java/com/mycompany/ch3/programmingProjectOne.java
@@ -0,0 +1,63 @@
+package com.mycompany.ch3;
+
+import java.util.Scanner;
+
+public class programmingProjectOne {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the first complex number: ");
+        double a = input.nextDouble();
+        double b = input.nextDouble();
+
+        Complex c1 = new Complex(a, b);
+
+        System.out.print("Enter the second complex number: ");
+        double c = input.nextDouble();
+        double d = input.nextDouble();
+
+        Complex c2 = new Complex(c, d);
+
+        System.out.println("("+c1 + ")"+ " + "+ "("+ c2 + ")"+ " = "+ c1.add(c2));
+        System.out.println("("+ c1 + ")"+ " -"+ "("+ c2 + ")"+ " = "+ c1.subtract(c2));
+        System.out.println("("+ c1 + ")"+ " * "+ "("+ c2 + ")"+ " = "+ c1.multiply(c2));
+        System.out.println("("+ c1 + ")"+ " / "+ "("+ c2 + ")"+ " = "+ c1.divide(c2));
+        System.out.println("|"+ c1 + "| = "+ c1.abs());
+
+        Complex c3 = (Complex)c1.clone();
+        System.out.println(c1 == c3);
+        System.out.println(c3.getRealPart());
+        System.out.println(c3.getImaginaryPart()); 
+    }
+
+    class Complex{
+        int realPart;
+        int imagePart;
+
+        public Complex(double a, double b){
+
+
+        }
+        public Complex(double a){
+
+
+        }
+        public Complex(){
+
+
+        }
+        public int getRealPart(){
+            return realPart;
+
+        }
+        public int getImaginaryPart(){
+            return imagePart;
+        }
+        public add(){
+            int sum = ;
+            return sum;
+
+        }
+        
+    }
+}
