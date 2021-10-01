7497bcd2673a2564fa4c91a69253339eeef174d6
zachhealy <zachjhealy@gmail.com>
2021-09-30 20:32:40 -0500

    commit on 'main'

--- CS256/ch1/src/main/java/com/mycompany/ch3/closeLab7/cLab7.java
+++ CS256/ch1/src/main/java/com/mycompany/ch3/closeLab7/cLab7.java
@@ -1,7 +1,29 @@
 package com.mycompany.ch3.closeLab7;
 
+import java.io.*;
+import java.util.*;
+
 public class cLab7 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a file for baby name ranking: ");
+        String fileInput = input.nextLine();
+
+        input = new Scanner(new File(fileInput));
+
+        Set<String> set1 = new HashSet<>();
+        Set<String> set2 = new HashSet<>();
+
+        while (input.hasNext()) {
+            input.nextInt();
+            set1.add(input.next());
+            input.nextInt();
+            set2.add(input.next());
+            input.nextInt();
+
+        }
+        System.out.println(set1);
 
     }
 
