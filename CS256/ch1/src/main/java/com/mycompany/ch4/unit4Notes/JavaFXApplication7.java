f44382d8e5ab67a8fd15001ef7733437d3e2d5e9
zachhealy <zachjhealy@gmail.com>
2021-10-04 12:38:54 -0500

    testing

--- CS256/ch1/src/main/java/com/mycompany/ch4/unit4Notes/JavaFXApplication7.java
+++ CS256/ch1/src/main/java/com/mycompany/ch4/unit4Notes/JavaFXApplication7.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.ch4.unit4Notes;;
+package com.mycompany.ch4.unit4Notes;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
