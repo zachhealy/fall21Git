4727187c7e34ff3446df803e10885aef59c5df88
zachhealy <zachjhealy@gmail.com>
2021-11-08 23:43:57 -0600

    Update arrAvg.s

--- CS288/ch1/Lab11/arrAvg.s
+++ CS288/ch1/Lab11/arrAvg.s
@@ -16,8 +16,28 @@
 .globl main
 
 main:
-    la $a0, arr
-    lw $a1, length
-    li $a2, 0
+    la $t0, arr
+    lw $t1, length
+    li $t2, 0 #Sum
+    li $t3, 0 #i
+    sumLoop:
+        lw $t4,  ($t0)
+        add $t2, $t2, $t4
+
+        add $t3, $t3, 1
+        add $t0, $t0, 4
+        blt $t3, $t1, sumLoop
+        sw $t2, sum
+
+    div $t5, $t2, $t1
+    sw $t5, average
+    mfhi $s1
+
+    li $v0, 1
+    move $a0, $t5
+    syscall
+
+    li $v0, 10
+    syscall
     
     
