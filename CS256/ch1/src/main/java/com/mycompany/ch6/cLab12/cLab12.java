f56346e9fe907b6da8acf8f8d08c8d7f827ab556
zachhealy <zachjhealy@gmail.com>
2021-11-09 13:26:41 -0600

    cLab 12 done?

--- CS256/ch1/src/main/java/com/mycompany/ch6/cLab12/cLab12.java
+++ CS256/ch1/src/main/java/com/mycompany/ch6/cLab12/cLab12.java
@@ -2,27 +2,30 @@ package com.mycompany.ch6.cLab12;
 
 import java.io.*;
 import java.util.*;
+import java.util.Queue;
 
 public class cLab12 {
     public static void main(String[] args) throws IOException {
-        BufferedReader read = new BufferedReader(new FileReader("/Users/zachhealy/Desktop/Programming Files/fall21Git/fall21Git/CS256/ch1/src/main/java/com/mycompany/ch4/closeLab10/palindrome.txt"));
-        BufferedWriter write = new BufferedWriter(new FileWriter("/Users/zachhealy/Desktop/Programming Files/fall21Git/fall21Git/CS256/ch1/src/main/java/com/mycompany/ch4/closeLab10/output.txt"));
+        BufferedReader read = new BufferedReader(new FileReader("/Users/zachhealy/Desktop/Programming Files/fall21Git/fall21Git/CS256/ch1/src/main/java/com/mycompany/ch6/cLab12/palindrome.txt"));
+        BufferedWriter write = new BufferedWriter(new FileWriter("/Users/zachhealy/Desktop/Programming Files/fall21Git/fall21Git/CS256/ch1/src/main/java/com/mycompany/ch6/cLab12/output.txt"));
         String str;
+        Queue queue = new LinkedList();
         while((str = read.readLine()) != null){
             str = str.toLowerCase().replaceAll("\\W", "");
-            for(int i=0; i<str.length(); i++){
-                stack.push(str.charAt(i));
+            for(int i = str.length() - 1; i > -1; i--){
+                queue.add(str.charAt(i));
             }
+            
             String reverse="";
-            while (!stack.isEmpty()){
-                reverse += stack.pop().toString();
+            while (!queue.isEmpty()){
+                reverse += queue.remove();
             }
             if(str.equals(reverse)){
                 write.write("Is a palindrome: " + reverse);
                 write.newLine();
             }
             else{
-                write.write("Not a palindrome");
+                write.write("Not a palindrome: " + reverse);
                 write.newLine();
             }
         }
