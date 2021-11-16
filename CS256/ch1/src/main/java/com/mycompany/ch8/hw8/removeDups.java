d098a5b06ce9296e485edbc01ff4c5a4b8efba7d
zachhealy <zachjhealy@gmail.com>
2021-11-15 23:39:37 -0600

    removeDups

---
+++ CS256/ch1/src/main/java/com/mycompany/ch8/hw8/removeDups.java
@@ -0,0 +1,27 @@
+package com.mycompany.ch8.hw8;
+
+import java.util.ArrayList;
+
+public class removeDups {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(10);
+        list.add(3);
+        list.add(3);
+        list.add(4);
+        list.add(7);
+        removeDupli(list);
+        System.out.println(list);
+
+    }
+    public static <E> ArrayList<E> removeDupli(ArrayList<E> list){
+        ArrayList<E> nList = new ArrayList<E>();
+        for(int i = 0; i < list.size(); i++){
+            if(!nList.contains(list.get(i))){
+                nList.add(list.get(i));
+            }
+        }
+        return nList;
+        
+    }
+}
