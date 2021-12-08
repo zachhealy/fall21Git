8d73b3b19ac6e575d10cf329f399dfbf5a189d17
zachhealy <zachjhealy@gmail.com>
2021-12-07 18:38:21 -0600

    Finished 288 Lab 14

--- CS288/ch1/Lab14/fibbo.s
+++ CS288/ch1/Lab14/fibbo.s
@@ -2,17 +2,18 @@
 # fibbo.s
 
 .data
-    called:   .asciiz "You called a function."
+    nLine:   .asciiz "\n"
 
 # code section
 .text
 .globl main
 
 main:
-    # Set the values for $a0 and $s1
+    #Set up registers for the base numbers as well as the amount we are going to need
     li $s0, 1
     li $s1, 1
-    li $s2, 20
+    li $s2, 19
+    la $s4, nLine
 
     jal func
 
@@ -20,26 +21,51 @@ main:
     syscall
 
 func:
+    #counter until we reach 20 prints
     li $t0, 0
 
-    la $a0, $s0
-    li $v0, 2
+    #Print first number
+    la $a0, 1
+    li $v0, 1
+    syscall
+    add $t0, $t0, 1
+
+    #print out new line
+    move $a0, $s4
+    li $v0, 4
     syscall
 
-    la $a0, $s1
-    li $v0, 2
+    #print second number
+    la $a0, 1
+    li $v0, 1
+    syscall
+    add $t0, $t0, 1
+
+    #print out new line
+    move $a0, $s4
+    li $v0, 4
     syscall
 
+    #loop to run through all numbers
     loop:
+        #add the numbers to get next number
         add $s3, $s0, $s1
 
-        la $a0, $s3
-        li $v0, 2
+        #print out new number
+        move $a0, $s3
+        li $v0, 1
         syscall
 
+        #move the past numbers to the new numbers
         move $s0, $s1
-        move $s1, $s3
+        move $s1, $a0
 
+        #print out new line
+        move $a0, $s4
+        li $v0, 4
+        syscall
+
+        #add to iterator and check if it is still in range
         add $t0, $t0, 1
         blt $t0, $s2, loop
 
